d72b86ce6ce51c82e6e7d09cede6b396ede3163f
TEYJUSDARSHAN <60425800+TEYJUSDARSHAN@users.noreply.github.com>
2021-03-05 12:37:47 +0530

    Initial commit

---
+++ Assets/user_input.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class user_input : MonoBehaviour
+{
+    public RectTransform right_half;
+    public RectTransform left_half;
+    float initial_scale;
+
+    private void Start()
+    {
+        initial_scale = right_half.localScale.x;
+    }
+
+    public void apply_flip_filter()
+    {
+        right_half.localScale = new Vector3(-initial_scale, initial_scale, initial_scale);
+        left_half.localScale = new Vector3(-initial_scale, initial_scale, initial_scale);
+    }
+
+    public void remove_flip_filter()
+    {
+        right_half.localScale = new Vector3(initial_scale, initial_scale, initial_scale);
+        left_half.localScale = new Vector3(initial_scale, initial_scale, initial_scale);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# vuforia_AR_project
+ A small project to implement basic functionalities of vuforia
